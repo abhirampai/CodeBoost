3bce0c862646bfa0591e18e9b20e94e3f140e4f0
Abhiram Pai <abhirampai1999@gmail.com>
2023-04-03 06:44:19 +0000

    Moved to unofficial api for chatgpt

--- src/Apis/refactorCode.js
+++ src/Apis/refactorCode.js
@@ -1,6 +1,16 @@
-import axios from "./axios";
+import { ChatGPTUnofficialProxyAPI } from "chatgpt";
+import Authenticator from "openai-authenticator";
 
-const createCompletion = (payload) =>
-  axios.post("https://api.openai.com/v1/chat/completions", payload);
+const authenticator = new Authenticator();
+
+const api = new ChatGPTUnofficialProxyAPI({
+  accessToken: authenticator.login(
+    process.env.REACT_APP_CHATGPT_EMAIL,
+    process.env.REACT_APP_CHATGPT_PASSWORD
+  ),
+  apiReverseProxyUrl: "https://bypass.churchless.tech/api/conversation",
+});
+
+const createCompletion = (payload) => api.send(payload);
 
 export default { createCompletion };
