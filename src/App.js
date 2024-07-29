14fa45c18c4b0545369caccdb7655d87c7f36e11
Abhiram Pai <abhirampai1999@gmail.com>
2024-07-29 11:56:43 +0000

    Added streaming ability with webllm

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import { QueryClient, QueryClientProvider } from "react-query";
 import Main from "./Components/Main";
 import "./styles.css";
+import { CreateWebWorkerMLCEngine } from "@mlc-ai/web-llm";
 
 const queryClient = new QueryClient();
+
+const selectedModel = "Phi-3-mini-4k-instruct-q4f16_1-MLC";
+
+const initProgressCallback = (initProgress) => {
+  console.log(initProgress);
+};
+
+const webLlmEngine = await CreateWebWorkerMLCEngine(
+  new Worker(new URL("./worker.js", import.meta.url), {
+    type: "module",
+  }),
+  selectedModel,
+  { initProgressCallback },
+);
 
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Main />
+      <Main webLlmEngine={webLlmEngine} />
     </QueryClientProvider>
   );
 }
